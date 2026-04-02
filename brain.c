4ab0db9c2f3d60d3b25a36c4ea195090a1f644d6
Filip Paciorek <filip_paciorek@wp.pl>
2026-04-02 23:09:58 +0200

    Transpose function completed, working on matrix multiplication

--- brain.c
+++ brain.c
@@ -8,7 +8,7 @@ matrix* transpose_matrix(matrix* mat);
 
 int main()
 {
-	matrix* mat = allocate_matrix(6,5);
+	matrix* mat = allocate_matrix(4,3);
 	fill_matrix(mat,0,100);
 	print_matrix(mat);
 	printf("\n-------------\n"); 
@@ -24,17 +24,43 @@ i64 time_diff(struct timespec a, struct timespec b)
 {
 	return ((b.tv_sec - a.tv_sec) * 1000000000LL + (b.tv_nsec - a.tv_nsec));
 }
+
 matrix* transpose_matrix(matrix* mat)
 {
 	u32 columns = mat->columns;
 	u32 rows = mat->rows;
+	u32 c1 = 0;
+	u32 c2 = 0;
 	matrix* mat_t = allocate_matrix(mat->columns,mat->rows);
-	for (u32 i = 0; i < columns; i++)
+	for (u32 i = 0; i < rows; i++)
 	{
-		for (u32 j = 0; j <rows; j++)
+		for (u32 j = 0; j <columns; j++)
 		{ 
-			mat_t->data[(rows*i) + j] = mat->data[(rows*j) + i];
+			u32 original_index = (i*columns) +j;
+			u32 transposed_index = (j*rows)+i;
+			mat_t->data[transposed_index] = mat->data[original_index];
 		}
 	}
 	return mat_t;
+}
+matrix* multiply_matrices(matrix* mat1, matrix* mat2)
+{
+	if (mat1->rows != mat2->columns && mat1->columns != mat2->rows)
+	{
+		printf("Wrong matrice dimensions!");
+		return NULL;
+	}
+	u32 rows = mat1->rows;
+	u32 columns = mat2->columns;
+	matrix* mult_mat = allocate_matrix(rows,rows);
+	
+	for (u32 i = 0; i < rows; i++)
+	{
+
+		for (u32 j =0; j < columns; j++)
+		{
+
+		}
+
+	}
 }
