6499038bb48f488fbd543229ab0fbadfd1448b83
Filip Paciorek <filip_paciorek@wp.pl>
2026-04-09 21:42:39 +0200

    Added initializaitons and activations, but still working out bugs,code operating but incorrectly

--- brain.c
+++ brain.c
@@ -12,6 +12,7 @@ typedef struct {
 	matrix* in;
 	matrix* W;
 	matrix* b;
+	matrix* z;
 	matrix* out;
 } Layer;
 
@@ -24,13 +25,17 @@ typedef struct {
 
 Layer* allocate_layer(u32 num_in, u32 num_out);
 void print_layer_weights(Layer* l);
+void print_layer_outputs(Layer* l);
 void calculate_layer(Layer* l1, matrix* input);
 void free_layer(Layer* l);
 Network* create_network_structure(u32* array,u32 num_layers);
 void print_network_params(Network* nn);
 void free_network(Network* nn);
+void b_initialization(Layer* l);
 void He_initialization(Layer* l);
-
+void Xavier_initialization(Layer* l);
+void ReLU_activation(Layer* l);
+void Sigmoid_activation(Layer* l);
 int main()
 {
 	//testing basic matrix arithmetic
@@ -63,13 +68,21 @@ int main()
 	Network* nn = create_network_structure(arr,3);
 	fill_matrix(nn->layers[0]->in,0,10);
 	print_network_params(nn);
-	for(int i=0; i < nn->num_layers;i++)
+	for(int i=0; i < nn->num_layers-1;i++)
 	{
 		printf("Layer %d \n", i);
+		b_initialization(nn->layers[i]);
 		He_initialization(nn->layers[i]);
 		calculate_layer(nn->layers[i],nn->layers[i]->in);
-		print_layer_weights(nn->layers[i]);
+		ReLU_activation(nn->layers[i]);
+		//print_layer_weights(nn->layers[i]);
+		//print_layer_outputs(nn->layers[i]);
+		print_matrix(nn->layers[i]->out);
 	};
+	b_initialization(nn->layers[nn->num_layers-1]);
+	Xavier_initialization(nn->layers[nn->num_layers-1]);
+	calculate_layer(nn->layers[nn->num_layers-1],nn->layers[nn->num_layers-1]->in);
+	Sigmoid_activation(nn->layers[nn->num_layers-1]);
 	free_network(nn);
 	return 1;
 }
@@ -83,6 +96,7 @@ Layer* allocate_layer(u32 num_in,u32 num_out)
 	l->in = allocate_matrix(l->num_in,1);
 	l->W = allocate_matrix(l->num_out,l->num_in);
 	l->b = allocate_matrix(l->num_out,1);
+	l->z = allocate_matrix(l->num_out,l->num_in);
 	l->out = allocate_matrix(l->num_out,1);
 	return l;
 }
@@ -98,10 +112,18 @@ void print_layer_weights(Layer* l)
 		printf("\n");
 	}
 }
+void print_layer_outputs(Layer* l)
+{
+	for(int i = 0; i < l->num_out; i++)
+	{
+		printf("%f",l->z[i]);
+	}
+	printf("\n");
+}
 void calculate_layer(Layer* l,matrix* input)
 {
 /* Calculate the layer by multiplication and point it to the output*/
-	multiply_matrices(l->W,input,l->out);
+	multiply_matrices(l->W,input,l->z);
 }
 
 void free_layer(Layer* l)
@@ -110,6 +132,7 @@ void free_layer(Layer* l)
 	free_matrix(l->in);
 	free_matrix(l->W);
 	free_matrix(l->b);
+	free_matrix(l->z);
 	free_matrix(l->out);
 	free(l);
 }
@@ -144,11 +167,26 @@ void free_network(Network* nn)
 	free(nn->layers);
 	free(nn);
 }
+void b_initialization(Layer* l)
+{
+	fill_matrix(l->b,0,1);
+}
 void He_initialization(Layer* l)
 {
 /* Use He initialization on all the weights of the layer*/
 	double limit = sqrtf((2.0 / l->num_in));
-	printf("%f \n",limit);
+	for (int i =0; i < l->W->rows;i++)
+	{
+		for(int j =0; j < l->W->columns;j++)
+		{
+			l->W->data[i*l->W->columns + j] = (((double)rand() /(double)RAND_MAX) * (2*limit))-limit;
+		}
+	}
+}
+void Xavier_initialization(Layer* l)
+{
+	u32 connections = l->num_in + l->num_out;
+	float limit = sqrtf(6.0/connections);
 	for (int i =0; i < l->W->rows;i++)
 	{
 		for(int j =0; j < l->W->columns;j++)
@@ -159,7 +197,25 @@ void He_initialization(Layer* l)
 }
 void ReLU_activation(Layer* l)
 {
-	//
+	for(int i = 0;i < l->num_out;i++)
+	{
+		if (l->z->data[i] > 0)
+		{
+			l->out->data[i] = l->z->data[i];
+		}
+     		else
+     		{
+			l->out->data[i] = 0;
+		}
+     
+	}
+}
+void Sigmoid_activation(Layer* l)
+{
+	for(int i = 0;i < l->num_out;i++)
+	{
+		l->out->data[i] = 1/(1+exp(-l->z->data[i]));
+	}
 }
 i64 time_diff(struct timespec a, struct timespec b)
 {
