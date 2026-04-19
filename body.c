54aa834473d1ed4513edeaddefa87b44efe0228a
busik <busik62busik@gmail.com>
2026-04-19 18:48:56 +0200

    Adding parse_csv_line function to body.c

--- body.c
+++ body.c
@@ -18,6 +18,28 @@ FILE * File_open(char *file_name){
 void file_close(FILE *fptr){
     fclose(fptr);
 }
-
+void parse_csv_line(char *line, matrix *X, matrix *Y, u32 row_index){
+    const char s[4]=",";
+    char *tok;
+    tok = strtok(line,s);
+    u32 col_index = 0;
+    while (tok!=NULL)
+    {   
+        double val = atof(tok);
+        if (col_index<8)
+        {
+            X->data[((row_index*8)+col_index)] = val;
+        }
+        else if (col_index==8)
+        {
+            Y->data[row_index] = val;
+        }
+        tok = strtok(NULL,s);
+        col_index+=1;
+        
+    }
+    
+    
+}
 
 
