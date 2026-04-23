0d564139b8b9ecf096f48806a6ab0d74c4dadfdb
busik <busik62busik@gmail.com>
2026-04-23 20:12:23 +0200

    Adding normalize function declaration

--- nn.h
+++ nn.h
@@ -40,4 +40,5 @@ FILE * File_open(char *file_name);
 void file_close(FILE *fptr);
 void parse_csv_line(char *line, matrix *X, matrix *Y, u32 row_index);
 void standardize_z_score(matrix *mat,matrix *mat_test);
+void normalize_min_max(matrix *mat,matrix *mat_test);
 #endif
